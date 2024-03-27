a7493386832b7e82275c89a962338db2dbb0b297
M1khail <mishalion888@gmail.com>
2024-03-27 19:18:37 +0100

    migration

---
+++ database/migrations/2024_03_27_124634_create_posts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+// 'php artisan make:model Post -m' -> command to create migration file and model(-m) file
+return new class extends Migration
+{
+    // method 'up' -> create the migration
+    public function up(): void // like sql query in Java
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('content'); // text bigger than string
+            $table->string('image')->nullable(); // string bc we save image url and possible null
+            $table->unsignedInteger('likes'); // only positive numbers
+            $table->boolean('is_published')->default(1); // 1 = true, 0 = false
+            $table->timestamps();
+        });
+    } // 'php artisan migrate' -> run migration
+
+
+    // method 'down' -> rollback the migration
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    } // 'php artisan migrate:rollback' -> reverse(rollback) the migration
+};
